b6afe7c92d82dee2e0474c750925a74d87f6b38b
SeokHo <djseokho@vip.qq.com>
2016-11-16 16:46:16 +0900

    SeokHo

---
+++ app/src/main/java/com/swein/shandroidtoolutils/example/ExampleSingtonClass.java
@@ -0,0 +1,25 @@
+package com.swein.shandroidtoolutils.example;
+
+/**
+ * Created by seokho on 16/11/2016.
+ */
+
+public class ExampleSingtonClass {
+
+//    private boolean isMyListFragmentShow = false;
+
+    private static ExampleSingtonClass instance = new ExampleSingtonClass();
+
+    public static ExampleSingtonClass getInstance() {
+        return instance;
+    }
+
+//    public void setIsMyListFragmentShow(boolean isMyListFragmentShow) {
+//        this.isMyListFragmentShow = isMyListFragmentShow;
+//    }
+//
+//    public boolean getIsMyListFragmentShow() {
+//        return this.isMyListFragmentShow;
+//    }
+
+}
