fe899cfac64f36d9a6510c622ff738ad065cdb59
SeokHo <djseokho@vip.qq.com>
2016-11-17 17:56:57 +0900

    SeokHo

---
+++ app/src/main/java/com/swein/shandroidtoolutils/tools/util/AppPackageUtils.java
@@ -0,0 +1,69 @@
+package com.swein.shandroidtoolutils.tools.util;
+
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+
+import java.util.List;
+
+/**
+ * Created by seokho on 17/11/2016.
+ */
+
+public class AppPackageUtils {
+
+    /**
+     * check APP and start APP
+     * @param context
+     * @param packagename
+     */
+    public static void startApplicationWithPackageName(Context context, String packagename) {
+
+        // get Activities, services, versioncode, name... from app package name
+        PackageInfo packageinfo = null;
+
+        try {
+            packageinfo = context.getPackageManager().getPackageInfo(packagename, 0);
+        }
+        catch ( PackageManager.NameNotFoundException e ) {
+            e.printStackTrace();
+        }
+
+        if ( packageinfo == null ) {
+
+            //should download or install app
+
+            return;
+        }
+
+        // create a Intent of package that type is CATEGORY_LAUNCHER
+        Intent resolveIntent = new Intent(Intent.ACTION_MAIN, null);
+        resolveIntent.addCategory(Intent.CATEGORY_LAUNCHER);
+        resolveIntent.setPackage(packageinfo.packageName);
+
+        // getPackageManager() queryIntentActivities
+        List<ResolveInfo> resolveinfoList = context.getPackageManager()
+                .queryIntentActivities(resolveIntent, 0);
+
+        ResolveInfo resolveinfo = resolveinfoList.iterator().next();
+
+        if ( resolveinfo != null ) {
+
+            String packageName = resolveinfo.activityInfo.packageName;
+            String className = resolveinfo.activityInfo.name;
+            Intent intent = new Intent(Intent.ACTION_MAIN);
+            intent.addCategory(Intent.CATEGORY_LAUNCHER);
+
+            ComponentName cn = new ComponentName(packageName, className);
+
+            intent.setComponent(cn);
+            context.startActivity(intent);
+
+            //System.exit(0);
+        }
+    }
+
+}
---
+++ app/src/main/java/com/swein/shandroidtoolutils/tools/util/DialogUtils.java
@@ -0,0 +1,129 @@
+package com.swein.shandroidtoolutils.tools.util;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+
+/**
+ * Created by seokho on 17/11/2016.
+ */
+
+public class DialogUtils {
+
+    public static void createNormalDialogWithOneButton(Context context, String title, String message, boolean cancelAble, String positiveButtonText) {
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(
+                context);
+
+        if(title == null || title.length() == 0) {
+
+        }
+
+        if(message == null || message.length() == 0) {
+
+        }
+
+        // set title
+        alertDialogBuilder.setTitle(title);
+
+        // set dialog message
+        alertDialogBuilder
+                .setMessage(message)
+                .setCancelable(cancelAble)
+                .setPositiveButton(positiveButtonText, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog,int id) {
+
+                        dialog.dismiss();
+                    }
+                });
+
+        // create alert dialog
+        AlertDialog alertDialog = alertDialogBuilder.create();
+
+        // show it
+        alertDialog.show();
+
+    }
+
+    public static void createNormalDialogWithTwoButton(Context context, String title, String message, boolean cancelAble, String positiveButtonText, String negativeButtonText) {
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(
+                context);
+
+        if(title == null || title.length() == 0) {
+
+        }
+
+        if(message == null || message.length() == 0) {
+
+        }
+
+        // set title
+        alertDialogBuilder.setTitle(title);
+
+        // set dialog message
+        alertDialogBuilder
+                .setMessage(message)
+                .setCancelable(cancelAble)
+                .setPositiveButton(positiveButtonText, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog,int id) {
+
+                        dialog.dismiss();
+                    }
+                })
+                .setNegativeButton(negativeButtonText, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog,int id) {
+                        dialog.cancel();
+                    }
+                });
+
+
+        // create alert dialog
+        AlertDialog alertDialog = alertDialogBuilder.create();
+
+        // show it
+        alertDialog.show();
+    }
+
+    public static void createNormalDialogWithThreeButton(Context context, String title, String message, boolean cancelAble, String positiveButtonText, String negativeButtonText, String otherButtonText) {
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(
+                context);
+
+        if(title == null || title.length() == 0) {
+
+        }
+
+        if(message == null || message.length() == 0) {
+
+        }
+
+        // set title
+        alertDialogBuilder.setTitle(title);
+
+        // set dialog message
+        alertDialogBuilder
+                .setMessage(message)
+                .setCancelable(cancelAble)
+                .setPositiveButton(positiveButtonText, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog,int id) {
+
+                        dialog.dismiss();
+                    }
+                })
+                .setNegativeButton(negativeButtonText, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog,int id) {
+                        dialog.cancel();
+                    }
+                })
+                .setNeutralButton(otherButtonText, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog,int id) {
+
+                    }
+                } );
+
+        // create alert dialog
+        AlertDialog alertDialog = alertDialogBuilder.create();
+
+        // show it
+        alertDialog.show();
+    }
+
+}
