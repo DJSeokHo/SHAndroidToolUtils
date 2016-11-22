bccbefb8d715ddc18a38d95576c5b09501600de5
SeokHo <djseokho@vip.qq.com>
2016-11-22 19:26:02 +0900

    SeokHo

---
+++ app/src/main/java/com/swein/storage/preferences/example/SharedPreferencesData.java
@@ -0,0 +1,41 @@
+package com.swein.storage.preferences.example;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
+
+/**
+ * Created by seokho on 22/11/2016.
+ */
+
+public class SharedPreferencesData {
+
+    private final String KEY = "com.swein.storage.preferences";
+
+    static Context context;
+
+    public SharedPreferencesData(Context context ) {
+        this.context = context;
+    }
+
+    public void putValue(String key, String value )
+    {
+        SharedPreferences pref = context.getSharedPreferences(KEY, Activity.MODE_PRIVATE);
+        SharedPreferences.Editor editor = pref.edit();
+        editor.putString(key, value);
+        editor.commit();
+    }
+
+
+    public String getValue(String key, String dftValue )
+    {
+        SharedPreferences pref = context.getSharedPreferences(KEY, Activity.MODE_PRIVATE);
+        try {
+            return pref.getString(key, dftValue);
+        }
+        catch (Exception e) {
+            return dftValue;
+        }
+    }
+
+}
