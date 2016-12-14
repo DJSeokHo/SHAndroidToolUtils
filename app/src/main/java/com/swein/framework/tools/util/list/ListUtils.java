2d9d2981e953257b9b08fe1f3d41e855d21f4ee6
SeokHo <djseokho@vip.qq.com>
2016-12-14 16:11:24 +0900

    SeokHo

---
+++ app/src/main/java/com/swein/framework/tools/util/list/ListUtils.java
@@ -0,0 +1,34 @@
+package com.swein.framework.tools.util.list;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * Created by seokho on 14/12/2016.
+ */
+
+public class ListUtils {
+
+    public static List removeDuplicateWithOrder(List list) {
+
+        Set set = new HashSet();
+
+        List newList = new ArrayList();
+
+        for ( Iterator iter = list.iterator(); iter.hasNext(); ) {
+
+            Object element = iter.next();
+
+            if ( set.add(element) ){
+                newList.add(element);
+            }
+
+        }
+        return newList;
+
+    }
+
+}
