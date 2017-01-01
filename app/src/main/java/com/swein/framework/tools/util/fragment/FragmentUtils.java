65f0dccd01beeed43ad17a302a1d9c962b988c34
SeokHo <djseokho@vip.qq.com>
2017-01-01 19:09:38 +0900

    SeokHo

--- app/src/main/java/com/swein/framework/tools/util/fragment/FragmentUtils.java
+++ app/src/main/java/com/swein/framework/tools/util/fragment/FragmentUtils.java
@@ -2,9 +2,14 @@ package com.swein.framework.tools.util.fragment;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.swein.framework.tools.util.debug.log.ILog;
 
 /**
  * Created by seokho on 01/01/2017.
@@ -15,5 +20,33 @@ public class FragmentUtils {
     public static View inflateFragment(LayoutInflater inflater, int layoutResource, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState, boolean attachToRoot) {
         View rootView = inflater.inflate(layoutResource, container, attachToRoot);
         return rootView;
+    }
+
+    public static void replaceFragmentv4Commit(android.support.v4.app.FragmentActivity activity, Fragment fragment, int containerViewId) {
+        FragmentManager fragmentManager = activity.getSupportFragmentManager();
+        fragmentManager.beginTransaction().replace(containerViewId, fragment).commit();
+    }
+
+    public void replaceFragmentv4CommitWithBundle(android.support.v4.app.FragmentActivity activity, Fragment fragment, int containerViewId, Bundle bundle) {
+
+        if(null == bundle) {
+            ILog.iLogException(FragmentUtils.class.getName(), "Input bundle first");
+            return;
+        }
+
+        FragmentManager fragmentManager = activity.getSupportFragmentManager();
+        fragment.setArguments(bundle);
+        fragmentManager.beginTransaction().replace(containerViewId, fragment).commit();
+
+    }
+
+    public static String getStringBundleDataFromActivity(Fragment fragment, String key) {
+        Bundle bundle = new Bundle();
+        bundle = fragment.getArguments();
+        if(null == bundle) {
+            ILog.iLogException(FragmentUtils.class.getName(), "Bundle is null");
+            return null;
+        }
+        return bundle.getString(key);
     }
 }
