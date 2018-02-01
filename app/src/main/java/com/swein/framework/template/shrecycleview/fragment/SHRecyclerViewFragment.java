b80791da59aa17719ddf531f7612114264204bae
seokho <djseokho@vip.qq.com>
2018-02-01 17:57:20 +0900

    tools util modify

--- app/src/main/java/com/swein/framework/template/shrecycleview/fragment/SHRecyclerViewFragment.java
+++ app/src/main/java/com/swein/framework/template/shrecycleview/fragment/SHRecyclerViewFragment.java
@@ -41,7 +41,6 @@ public class SHRecyclerViewFragment extends Fragment {
 
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
