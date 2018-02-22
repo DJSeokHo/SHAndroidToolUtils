cc1ed9e3025884703a7adeebebd4e352b127116c
seokho <djseokho@vip.qq.com>
2018-02-22 15:15:02 +0900

    - GCM Push bug 수정 - add Activity visible & in-visible UI update

--- app/src/main/java/com/swein/framework/module/gcmpush/broadcast/NotificationBroadcastReceiver.java
+++ app/src/main/java/com/swein/framework/module/gcmpush/broadcast/NotificationBroadcastReceiver.java
@@ -10,9 +10,16 @@ import com.swein.framework.module.gcmpush.constants.GCMConstants;
 import com.swein.framework.tools.util.toast.ToastUtil;
 
 /**
+ *
+ * ********* add this part in your AndroidManifest.xml *********
+     <receiver android:name=".push.broadcast.NotificationBroadcastReceiver">
+         <intent-filter>
+            <action android:name="com.android.broadcast.RECEIVER_ACTION"/>
+         </intent-filter>
+     </receiver>
+
  * Created by seokho on 22/02/2018.
  */
-
 public class NotificationBroadcastReceiver extends BroadcastReceiver{
 
     @Override
@@ -24,18 +31,21 @@ public class NotificationBroadcastReceiver extends BroadcastReceiver{
             return;
         }
 
+        /*
+            Activity is running...
+         */
         String message = bundle.getString(GCMConstants.NOTIFICATION_CLICKED_KEY);
-
         if(GoogleCloudMessageActivity.visible) {
             ToastUtil.showCustomShortToastNormal(context, message);
             return;
         }
 
-
+        /*
+            Activity is not running
+         */
         Intent activityIntent = new Intent(context, GoogleCloudMessageActivity.class);
         activityIntent.putExtra(GCMConstants.NOTIFICATION_CLICKED_BUNDLE_KEY, bundle);
         activityIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         context.startActivity(activityIntent);
-
     }
 }
