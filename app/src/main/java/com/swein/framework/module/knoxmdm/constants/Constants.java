43fe410bd5d9786a9ba8c0ed45456ba0b3d2d574
seokho <djseokho@vip.qq.com>
2018-08-02 16:33:20 +0900

    add fullScreenBackwardCompatibility util

--- app/src/main/java/com/swein/framework/module/knoxmdm/constants/Constants.java
+++ app/src/main/java/com/swein/framework/module/knoxmdm/constants/Constants.java
@@ -3,10 +3,10 @@ package com.swein.framework.module.knoxmdm.constants;
 public class Constants {
 
     /* This key currently only supports Knox 2.8+ devices. If devices are running Knox 2.7 and earlier, you must also activate the backwards-compatible key. */
-    public static final String KNOX_LICENSE_KEY = "";
+    public static final String KNOX_LICENSE_KEY = "KLM06-HX97U-ZHBRX-05I6N-JYCL5-0Z9YL";
 
     /* Use this key if you plan to deploy your solutions on devices running Knox 2.7 or earlier. This key provides no permissions, but needs to be activated along with a Samsung Knox license key in order to get the required permissions from the Samsung License Server. */
-    public static final String BACKWARD_LICENSE_KEY = "";
+    public static final String BACKWARD_LICENSE_KEY = "848906AFB7E699A5926BBA01F1806814B3AA475E2C9582B23A8602F28C8ACDB865F5D707A7C2BC5223507A29350CFA4356B28C594899018D90A3D37CD49E5F6E";
 
     public static final int KNOX_VERSION_CODES_BASE = 6;
     public static final int KNOX_VERSION_CODES1_0_1 = 7;
--- app/src/main/java/com/swein/framework/tools/util/window/WindowUtil.java
+++ app/src/main/java/com/swein/framework/tools/util/window/WindowUtil.java
@@ -1,6 +1,7 @@
 package com.swein.framework.tools.util.window;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Build;
 import android.view.View;
 import android.view.Window;
@@ -8,6 +9,48 @@ import android.view.WindowManager;
 
 public class WindowUtil {
 
+    /**
+     * android full screen above 4.4
+     * @param activity activity
+     */
+    public static void fullScreenBackwardCompatibility(Activity activity) {
+
+        if(Build.VERSION.SDK_INT < Build.VERSION_CODES.KITKAT) {
+            return;
+        }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+
+            Window window = activity.getWindow();
+            View decorView = window.getDecorView();
+
+            int option = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
+            decorView.setSystemUiVisibility(option);
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(Color.TRANSPARENT);
+
+//                window.setNavigationBarColor(Color.TRANSPARENT);
+        }
+        else {
+
+            Window window = activity.getWindow();
+            WindowManager.LayoutParams attributes = window.getAttributes();
+            int flagTranslucentStatus = WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS;
+            int flagTranslucentNavigation = WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION;
+            attributes.flags |= flagTranslucentStatus;
+//                attributes.flags |= flagTranslucentNavigation;
+            window.setAttributes(attributes);
+
+        }
+    }
+
+    /**
+     *
+     * android full screen above 5.0
+     *
+     * @param activity
+     */
     public static void fullScreen(Activity activity) {
         // full screen
         if ( Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT )
