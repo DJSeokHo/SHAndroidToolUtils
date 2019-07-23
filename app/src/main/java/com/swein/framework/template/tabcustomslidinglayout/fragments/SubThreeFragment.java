e1ca91c849ec477dd72bb509d601ac670bc54856
DJSeokHo <djseokho@vip.qq.com>
2019-07-23 11:11:29 +0900

    add tab layout

---
+++ app/src/main/java/com/swein/framework/template/tabcustomslidinglayout/fragments/SubThreeFragment.java
@@ -0,0 +1,30 @@
+package com.swein.framework.template.tabcustomslidinglayout.fragments;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.swein.shandroidtoolutils.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class SubThreeFragment extends Fragment {
+
+
+    public SubThreeFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_sub_three, container, false);
+    }
+
+}
--- app/src/main/java/com/swein/shandroidtoolutils/MainActivity.java
+++ app/src/main/java/com/swein/shandroidtoolutils/MainActivity.java
@@ -24,6 +24,7 @@ import com.swein.framework.module.camera.custom.camera2.fragment.CameraTwoFragme
 import com.swein.framework.module.camera.system.SHCameraIntent;
 import com.swein.framework.module.camera.system.SystemCameraDemoActivity;
 import com.swein.framework.module.googleanalytics.aop.monitor.processtimer.TimerTrace;
+import com.swein.framework.template.tabcustomslidinglayout.activity.TabCustomSlidingLayoutActivity;
 import com.swein.framework.tools.util.activity.ActivityUtil;
 import com.swein.framework.tools.util.animation.AnimationUtil;
 import com.swein.framework.tools.util.appinfo.AppInfoUtil;
@@ -266,7 +267,8 @@ public class MainActivity extends Activity {
 //        ActivityUtil.startNewActivityWithoutFinish(this, NestedScrollWithFixedBarActivity.class);
 //        ActivityUtil.startNewActivityWithoutFinish(MainActivity.this, HandlerThreadTemplateActivity.class);
 //        ActivityUtil.startNewActivityWithoutFinish(MainActivity.this, SystemCameraDemoActivity.class);
-        ActivityUtil.startNewActivityWithoutFinish(MainActivity.this, CameraTwoActivity.class);
+//        ActivityUtil.startNewActivityWithoutFinish(MainActivity.this, CameraTwoActivity.class);
+        ActivityUtil.startNewActivityWithoutFinish(MainActivity.this, TabCustomSlidingLayoutActivity.class);
 
 
 
