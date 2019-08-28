7a7c032d1823b8ec8a13e6e844e33069a9a50cfd
DJSeokHo <djseokho@vip.qq.com>
2019-08-28 18:52:41 +0900

    add util

--- app/src/main/java/com/swein/framework/tools/util/display/DisplayUtils.java
+++ app/src/main/java/com/swein/framework/tools/util/display/DisplayUtils.java
@@ -1,13 +1,41 @@
 package com.swein.framework.tools.util.display;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.res.Configuration;
+import android.view.WindowManager;
 
 /**
  * Created by seokho on 21/06/2017.
  */
 
 public class DisplayUtils {
+
+    /**
+     * put is in onResume() or onCreate
+     * @param activity
+     */
+    public static void keepScreenOn(Activity activity) {
+        activity.getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
+    }
+
+    /**
+     * put this in onPause() or onDestroy()
+     * @param activity
+     */
+    public static void unKeepScreenOn(Activity activity) {
+        activity.getWindow().clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+    }
+
+    public static int getStatusBarHeight(Context context) {
+        int result = 0;
+        int resourceId = context.getResources().getIdentifier("status_bar_height", "dimen", "android");
+        if (resourceId > 0) {
+            result = context.getResources().getDimensionPixelSize(resourceId);
+        }
+        return result;
+    }
 
     public static boolean isLandscape(Context context) {
         if(context.getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
