0a38ac90b71233584eec7dc0197dd6cc23472a46
DJSeokHo <djseokho@vip.qq.com>
2019-08-29 13:19:44 +0900

    add custom view

--- app/src/main/java/com/swein/framework/template/customviewcontroller/demo/CustomViewControllerActivity.java
+++ app/src/main/java/com/swein/framework/template/customviewcontroller/demo/CustomViewControllerActivity.java
@@ -2,15 +2,41 @@ package com.swein.framework.template.customviewcontroller.demo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.FrameLayout;
 
+import com.swein.framework.template.customviewcontroller.NavigationViewController;
 import com.swein.shandroidtoolutils.R;
 
 public class CustomViewControllerActivity extends AppCompatActivity {
+
+    private FrameLayout frameLayoutNavigation;
+
+    private NavigationViewController navigationViewController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_custom_view_controller);
 
+        frameLayoutNavigation = findViewById(R.id.frameLayoutNavigation);
+
+        navigationViewController = new NavigationViewController(this);
+        navigationViewController.setTitle("haha");
+        frameLayoutNavigation.addView(navigationViewController);
+
+    }
+
+    @Override
+    public void onBackPressed() {
+
+        if(navigationViewController != null) {
+
+            frameLayoutNavigation.removeView(navigationViewController);
+            navigationViewController = null;
+
+            return;
+        }
+
+        finish();
     }
 }
