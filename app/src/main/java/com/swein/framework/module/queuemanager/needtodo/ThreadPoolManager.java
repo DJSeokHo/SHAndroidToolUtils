597211f4501b7f6b4e50370f1b8e13658771e47f
seokho <djseokho@vip.qq.com>
2019-10-19 16:01:21 +0900

    add permission template

--- app/src/main/java/com/swein/framework/module/queuemanager/needtodo/ThreadPoolManager.java
+++ app/src/main/java/com/swein/framework/module/queuemanager/needtodo/ThreadPoolManager.java
@@ -3,6 +3,7 @@ package com.swein.framework.module.queuemanager.needtodo;
 import android.util.Log;
 
 import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.DelayQueue;
 import java.util.concurrent.LinkedBlockingDeque;
 import java.util.concurrent.RejectedExecutionHandler;
 import java.util.concurrent.ThreadPoolExecutor;
@@ -21,7 +22,9 @@ public class ThreadPoolManager {
     private LinkedBlockingDeque<Runnable> queue = new LinkedBlockingDeque<>();
 
     // 还需要一个失败的任务的队列，以便重试
+    // 丢进去的任务需要指定延迟时间，和重试次数
     // TODO 失败队列
+    private DelayQueue delayQueue = new DelayQueue<>();
 
     // 将请求放入队列
     public void addTask(Runnable runnable) {
@@ -74,6 +77,32 @@ public class ThreadPoolManager {
         }
     };
 
+    // 创建延时调度线程
+    public Runnable delayThread = new Runnable() {
 
+        Runnable runnable;
+
+        @Override
+        public void run() {
+            while (true) {
+                try {
+                    runnable = queue.take();
+                    /*
+                        if 任务的次数 < 3
+                            可以再重试 threadPoolExecutor.execute(任务);
+                            任务的次数 +1
+                     */
+
+                }
+                catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                if(runnable != null) {
+                    threadPoolExecutor.execute(runnable);
+                }
+            }
+        }
+    };
 
 }
