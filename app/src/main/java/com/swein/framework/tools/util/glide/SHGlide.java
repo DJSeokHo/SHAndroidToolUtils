117bab917a3e33db26ed4e5c4a36d73e42d7d43c
DJSeokHo <djseokho@gmail.com>
2020-02-24 00:30:13 +0900

    add naver login

--- app/src/main/java/com/swein/framework/tools/util/glide/SHGlide.java
+++ app/src/main/java/com/swein/framework/tools/util/glide/SHGlide.java
@@ -10,6 +10,7 @@ import androidx.annotation.Nullable;
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.RequestBuilder;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.request.RequestOptions;
 
 import static com.bumptech.glide.load.resource.bitmap.BitmapTransitionOptions.withCrossFade;
 
@@ -22,7 +23,7 @@ public class SHGlide {
     }
 
 
-    public void setImageBitmap(Context context, String url, ImageView imageView, @Nullable Drawable placeHolder, int width, int height, float thumbnailSize) {
+    public void setImageBitmap(Context context, String url, ImageView imageView, @Nullable Drawable placeHolder, int width, int height, float rate, float thumbnailSize) {
 
         RequestBuilder<Bitmap> requestBuilder = Glide.with(context).asBitmap().load(url).transition(withCrossFade());
         if(placeHolder != null) {
@@ -30,6 +31,12 @@ public class SHGlide {
         }
 
         if(width != 0 && height != 0) {
+
+            if(rate != 0) {
+                width = (int)((float)width * rate);
+                height = (int)((float)height * rate);
+            }
+
             requestBuilder = requestBuilder.override(width, height);
         }
 
@@ -38,6 +45,56 @@ public class SHGlide {
         }
 
         requestBuilder.skipMemoryCache(true).diskCacheStrategy(DiskCacheStrategy.AUTOMATIC).into(imageView);
+
+    }
+
+    public void setImageBitmap(Context context, int imageResource, ImageView imageView, @Nullable Drawable placeHolder, int width, int height, float rate, float thumbnailSize) {
+
+        RequestBuilder<Bitmap> requestBuilder = Glide.with(context).asBitmap().load(imageResource).transition(withCrossFade());
+        if(placeHolder != null) {
+            requestBuilder = requestBuilder.placeholder(placeHolder);
+        }
+
+        if(width != 0 && height != 0) {
+
+            if(rate != 0) {
+                width = (int)((float)width * rate);
+                height = (int)((float)height * rate);
+            }
+
+            requestBuilder = requestBuilder.override(width, height);
+        }
+
+        if(thumbnailSize != 0) {
+            requestBuilder = requestBuilder.thumbnail(thumbnailSize);
+        }
+
+        requestBuilder.skipMemoryCache(true).diskCacheStrategy(DiskCacheStrategy.AUTOMATIC).into(imageView);
+
+    }
+
+    public void setRoundedImageBitmap(Context context, String url, ImageView imageView, @Nullable Drawable placeHolder, int width, int height, float rate, float thumbnailSize) {
+
+        RequestBuilder<Bitmap> requestBuilder = Glide.with(context).asBitmap().load(url).transition(withCrossFade());
+        if(placeHolder != null) {
+            requestBuilder = requestBuilder.placeholder(placeHolder);
+        }
+
+        if(width != 0 && height != 0) {
+
+            if(rate != 0) {
+                width = (int)((float)width * rate);
+                height = (int)((float)height * rate);
+            }
+
+            requestBuilder = requestBuilder.override(width, height);
+        }
+
+        if(thumbnailSize != 0) {
+            requestBuilder = requestBuilder.thumbnail(thumbnailSize);
+        }
+
+        requestBuilder.skipMemoryCache(true).diskCacheStrategy(DiskCacheStrategy.AUTOMATIC).apply(RequestOptions.circleCropTransform()).into(imageView);
 
     }
 
